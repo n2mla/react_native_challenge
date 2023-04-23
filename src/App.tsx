79aee2edda76778e3e6c552830997fb855522aef
n2mla <{ID}+{username}@users.noreply.github.com>
2023-04-24 00:52:02 +0530

    Add scrollView

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 import Model from './models/Model'
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <View style={styles.container}>
-        <Model />
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <Model />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
